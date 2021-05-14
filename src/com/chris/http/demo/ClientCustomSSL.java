e2b1f8c5efc34c4209cd7297e62e34c5fc9b4b9c
Chris Gulledge <chrandgull@gmail.com>
2021-05-13 20:36:15 -0400

    Working example of passing https through a proxy

--- src/com/chris/http/demo/ClientCustomSSL.java
+++ src/com/chris/http/demo/ClientCustomSSL.java
@@ -3,13 +3,18 @@ package com.chris.http.demo;
 import java.io.File;
 import javax.net.ssl.SSLContext;
 import org.apache.http.HttpEntity;
+import org.apache.http.HttpHost;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.conn.routing.HttpRoutePlanner;
 import org.apache.http.conn.ssl.NoopHostnameVerifier;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.DefaultProxyRoutePlanner;
 import org.apache.http.ssl.SSLContextBuilder;
 import org.apache.http.ssl.SSLContexts;
 import org.apache.http.util.EntityUtils;
@@ -18,6 +23,16 @@ public class ClientCustomSSL {
    
    public final static void main(String[] args) throws Exception {
 
+	   
+	  //Creating an HttpHost object for proxy
+	  HttpHost proxyhost = new HttpHost("localhost",8008);
+	   
+      //Creating an HttpHost object for target
+      HttpHost targethost = new HttpHost("ptsv2.com",443);
+	  
+      //creating a RoutePlanner object
+      HttpRoutePlanner routePlanner = new DefaultProxyRoutePlanner(proxyhost);
+      
       //Creating SSLContextBuilder object
       SSLContextBuilder SSLBuilder = SSLContexts.custom();
   
@@ -34,6 +49,7 @@ public class ClientCustomSSL {
  
       //Creating HttpClientBuilder
       HttpClientBuilder clientbuilder = HttpClients.custom();
+      clientbuilder = clientbuilder.setRoutePlanner(routePlanner);
 
       //Setting the SSLConnectionSocketFactory
       clientbuilder = clientbuilder.setSSLSocketFactory(sslConSocFactory);
@@ -41,11 +57,19 @@ public class ClientCustomSSL {
       //Building the CloseableHttpClient
       CloseableHttpClient httpclient = clientbuilder.build();
       
-      //Creating the HttpGet request
-      HttpGet httpget = new HttpGet("https://example.com/");
+      //Create a string entity
+      
+      StringEntity se = new StringEntity("test=myvar");
+      
+      //Creating the HttpPost request
+      HttpPost httpPost = new HttpPost("https://ptsv2.com/t/5ldkf-1620949253/post");
+      
+      //Associate the entity with the httpPost object
+      
+      httpPost.setEntity(se);
  
       //Executing the request
-      HttpResponse httpresponse = httpclient.execute(httpget);
+      HttpResponse httpresponse = httpclient.execute(targethost,httpPost);
 
       //printing the status line
       System.out.println(httpresponse.getStatusLine());
@@ -53,7 +77,7 @@ public class ClientCustomSSL {
       //Retrieving the HttpEntity and displaying the no.of bytes read
       HttpEntity entity = httpresponse.getEntity();
       if (entity != null) {
-         System.out.println(EntityUtils.toByteArray(entity).length);
+         System.out.println(EntityUtils.toString(entity));
       } 
    }
 }
